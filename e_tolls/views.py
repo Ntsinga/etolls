0c680f15ba5dcdedfd417d1488e84ee38bc8b14a
Ntsinga <elijahntsinga@gmail.com>
2023-07-14 20:10:49 +0300

    Firebase

--- e_tolls/views.py
+++ e_tolls/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import render
-# import firebase_admin
-# from firebase_admin import credentials
-# from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase import firebase
 
-
-# cred=credentials.Certificate('./ServiceAccountKey.json')
-# default_app=firebase_admin.initialize_app(cred)
-# db=firestore.client()
-# firebase=db.FirebaseApplication('',None)
+cred=credentials.Certificate('e_tolls/serviceAccountKey.json')
+default_app=firebase_admin.initialize_app(cred)
+db=firestore.client()
+firebase=firebase.FirebaseApplication("https://etolls-8d93b-default-rtdb.firebaseio.com",None)
 
 def verify(request):
-    # userID= database.child('User Data').child('userID').get().val()
-    #     stack = database.child('Data').child('Stack').get().val()
-    #     framework = database.child('Data').child('Framework').get().val()
+
     
+    # doc = db.collection('users').document('yMtsidNmTFUsk3CyS3rtGOruo8v1').get()
+    # s=doc.to_dict()
+    # print(s)
+
+    # db.collection("users").document("yMtsidNmTFUsk3CyS3rtGOruo8v1").set({
+    #     "email":"benj1234@gmail.com",
+    #     "name":"Ssempala"
+    # })
+    # db.collection("users").document("bro").set({
+    #     "email":"benj1234@gmail.com",
+    #     "name":"Ssempala"
+    # })
+
+    return render(request,"index.html",status=404)
+  
     # deducted_credits=2
     # user =User.objects.get(id=user_id)
 
     # if userID is None:
-    return render(request,"index.html",status=404)
+    
 
     # if(user.available_credits<deducted_credits):
     #     #return error message
