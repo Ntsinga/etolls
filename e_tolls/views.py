9241511dc33d9722d5a7a9d0f01128e4d01f3f99
Ntsinga <68024619+Ntsinga@users.noreply.github.com>
2023-07-18 17:24:45 +0300

    Clean

--- e_tolls/views.py
+++ e_tolls/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import User,Transaction
+
 
 # Create your views here.
 
 def verify(request):
     
-    # deducted_credits=2
-    # user =User.objects.get(id=user_id)
 
-    # if user is None:
-    return HttpResponse('Success',content_type=text/plain)
-    # return render(request,"index.html",status=200)
-
-    # if(user.available_credits<deducted_credits):
-    #     #return error message
-    #     return HttpResponse(status=403)
+    return HttpResponse('Success',content_type='text/plain')
     
-    # #Create transaction object
-    # Transaction.objects.create()
-    # user.available_credits-=deducted_credits
-    # user.save()
-    # return HttpResponse()
